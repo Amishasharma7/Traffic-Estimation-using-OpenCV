bcbfd6077db8d65192f584ce38759b7b5e53a203
Amishasharma7 <amishasharma516@gmail.com>
2025-08-04 23:26:19 +0530

    Added full project files

---
+++ Traffic-Estimation-using-openCV.py
@@ -0,0 +1,32 @@
+import cv2
+import imutils
+cascade_src = 'cars.xml'
+car_cascade = cv2.CascadeClassifier(cascade_src)
+cam = cv2.VideoCapture(0)
+
+while True:
+    detected = 0
+    _,img = cam.read()
+    img = imutils.resize(img,width=300)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+    for (x,y,w,h) in cars:
+        cv2.rectangle(img, (x,y), (x+w,y+h),(0,0,255),2)
+    cv2.imshow("Frame",img)
+    b = str(len(cars))
+    a = int(b)
+    detected = a
+    n = detected
+    print("------------------------------------------")
+    print("North: %d"%(n))
+    if n>=2:
+        print("North More Traffic")
+    else:
+        print("no traffic")
+    if cv2.waitKey(33) == 27:
+        break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
